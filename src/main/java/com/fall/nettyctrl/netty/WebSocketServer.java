96b0be5e5db9e7d589b75446f29bdd686ab54b16
FAll <940313262@qq.com>
2024-06-13 09:33:42 +0800

    update configuration meta

--- src/main/java/com/fall/nettyctrl/netty/WebSocketServer.java
+++ src/main/java/com/fall/nettyctrl/netty/WebSocketServer.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 
 /**
  * WebSocket服务器
