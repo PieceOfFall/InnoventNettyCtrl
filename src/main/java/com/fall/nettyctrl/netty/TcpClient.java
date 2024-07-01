d324a3db50a88112f14b6e7aeb29689b6d0724ee
FAll <940313262@qq.com>
2024-07-01 14:34:16 +0800

    feat: 完成报错设备号问题

--- src/main/java/com/fall/nettyctrl/netty/TcpClient.java
+++ src/main/java/com/fall/nettyctrl/netty/TcpClient.java
@@ -15,12 +15,17 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * @author FAll
@@ -28,13 +33,21 @@ import java.net.InetSocketAddress;
  */
 @Slf4j
 @Component
-@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "ip-name")
 public class TcpClient {
 
     @Value("${netty.tcp-client.timeout}")
     private Integer timeout;
 
+    @Setter
+    private LinkedHashMap<String,String> map;
+
     private final ResponseUtil responseUtil;
+
+    @Autowired
+    public TcpClient(ResponseUtil responseUtil) {
+        this.responseUtil = responseUtil;
+    }
 
     @Async
     public void sendMsg(String ip, int port, String message) {
@@ -57,8 +70,7 @@ public class TcpClient {
             channel.writeAndFlush(buffer).sync();
             channel.closeFuture();
         } catch (Exception e) {
-            WebSocketServerHandler.broadcastMessage(responseUtil.error(STR."\{ip}:\{port} \n[\{message}]后端发送指令异常"));
-
+            WebSocketServerHandler.broadcastMessage(responseUtil.info(STR."后端发送[\{map.get(ip)}]指令异常"));
             log.error("Error while sending TCP message: ", e);
             Thread.currentThread().interrupt();
         } finally {
